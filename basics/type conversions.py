6e6be17328b64833192440470d5e6270a78192e7
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-06 19:25:52 +0530

    Add type conversion practice

---
+++ basics/type conversions.py
@@ -0,0 +1,18 @@
+# Implicit Type Conversion (Automatic)
+
+a = 10
+b = 6.6
+
+c = a + b
+
+print(c)
+print(type(c))
+
+# Explicit Type Conversion / Tpye Casting (Manual)
+
+a = int("2")
+b = 4
+
+sum =  a + b
+print(sum)
+print(type(sum))
