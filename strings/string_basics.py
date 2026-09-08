f6b136b850cda1d9f79fdd52266365150d64d88b
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-08 21:33:52 +0530

    Add basic string practice

---
+++ strings/string_basics.py
@@ -0,0 +1,15 @@
+# String
+
+name = "Anshu Singh"
+city = "Indore"
+
+print("My name is ", name)
+print("I live in ", city)
+
+# Multi Line String
+
+message = """Hello!
+I am learning python.
+Python is easy to learn."""
+
+print(message)
