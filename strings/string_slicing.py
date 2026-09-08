79c1f971dae32778a97dc0e343df7c875056e958
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-08 21:35:23 +0530

    Add string slicing practice

---
+++ strings/string_slicing.py
@@ -0,0 +1,19 @@
+# String Indexing
+
+str = "Anshu Singh"
+
+print(str[0:5])
+print(str[6:10])
+
+# From beginnig
+print(str[:8])
+
+# Until the end
+print(str[6:])
+print(str[5:len(str)])
+
+# Every second character
+print(str[::2])
+
+# Reverse string
+print(str[::-1])
