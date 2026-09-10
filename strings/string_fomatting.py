c35da6159447e0c8e30cbd917060c0b9d5039872
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-10 18:37:32 +0530

    Add string formatting practice

---
+++ strings/string_fomatting.py
@@ -0,0 +1,10 @@
+# String Formatting
+
+name = "Anshu"
+age = 18
+language = "Python"
+
+# f-string
+message = f"My name is {name}. I am {age} years old and I am learning {language}."
+
+print(message)
