502f135bb5ddd50ec12d7a19174bde3c4902b538
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-10 18:36:38 +0530

    Add string checking practice

---
+++ strings/string_checking.py
@@ -0,0 +1,14 @@
+# String Checking
+
+str1 = "Python"
+str2 = "12345"
+str3 = 'Pyhton123'
+
+print(str1.isalpha())
+print(str2.isdigit())
+print(str3.isalnum())
+print(str2.isalpha())
+
+str4 = "pyhton"
+print(str4.islower())
+print(str4.isupper())
