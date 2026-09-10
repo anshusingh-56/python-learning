daee4f284fd8a36293661ea9745ff2b1f6dd7e7e
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-10 18:36:03 +0530

    Add escape sequences practice

---
+++ strings/escape_sequences.py
@@ -0,0 +1,14 @@
+# New line
+print("Hello\nWorld")
+
+# Tab
+print("Name:\tAnshu")
+
+# Double quotes inside string
+print("He said, \"Python is easy.\"")
+
+# Single quotes inside string
+print('It\'s a Python program.')
+
+# Backslash
+print("C:\\Pyhon\\Programs")
