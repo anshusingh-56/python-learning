7d719b029b9de661bb6bf1b838cbd0dc312c191a
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-10 18:38:17 +0530

    Add string methodds practice

---
+++ strings/string_methods.py
@@ -0,0 +1,18 @@
+# String Methods
+
+str = "Anshu Singh "
+
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.title())
+print(str.count("h"))
+print(str.find("g"))
+
+# Replace text
+message = "I like Java"
+print(message.replace("Java","Python"))
+
+# Remove extra space
+name = "    Anshu    "
+print(name.strip())
