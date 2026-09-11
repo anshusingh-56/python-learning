572b6b815eb38c869386153ccf49b0876ac6e998
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-11 18:19:28 +0530

    Add list basic practice

---
+++ lists/list_basics.py
@@ -0,0 +1,14 @@
+fruits = ["Apple", "Banana","Mango","Orange"]
+
+print(fruits)
+print("First Fruit:", fruits[0])
+print("Total Fruits:", len(fruits))
+
+# it can store different types of elements.
+
+student = ["Anshu", 18, "Indore"]
+
+print(student)
+print(student[0])
+print(student[1])
+print(student[2])
