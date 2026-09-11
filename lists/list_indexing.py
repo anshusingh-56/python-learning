17890c6e306e94ac2c8ad717a1853844a670350a
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-11 18:22:11 +0530

    Add list indexing practice

---
+++ lists/list_indexing.py
@@ -0,0 +1,13 @@
+# Positive indexing (Start from 0)
+nums = [10, 20, 30, 40, 50]
+
+print("First number:", nums[0])
+print("Second number:", nums[1])
+print("Third number:", nums[2])
+print("Fourth number:", nums[3])
+print("Fifth number:", nums[4])
+
+# Negative indexing (Star from -1)
+print("Last number:", nums[-1])
+print("Second last number:", nums[-2])
+print("Third last number:", nums[-3])
