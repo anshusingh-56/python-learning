400e565ded93669f4d92fb87a446d06c0d1a39d4
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-14 20:02:12 +0530

    Add tuple basics practice
    Demonstrate tuple creation and properties in Python.

---
+++ tuples_and_sets/tuple_basics.py
@@ -0,0 +1,20 @@
+tup = (2, 4, 5, 7, 3)
+
+print(tup)
+print("Length:", len(tup))
+print("Type:", type(tup))
+
+# empty () is a tuple
+num = ()
+print(num)
+print(type(num))
+
+# When single element inside a () without , is a integer
+numbers = (20)
+print(numbers)
+print(type(numbers))
+
+# When sigle element inside a () with , is a tuple
+numbers = (10,)
+print(numbers)
+print(type(numbers))
