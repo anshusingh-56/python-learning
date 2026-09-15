c5e1a3e17ca275c182685a398e27f8ef99ba45d6
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-15 17:45:26 +0530

    Add set methods practice

---
+++ tuples_and_sets/set_methods.py
@@ -0,0 +1,13 @@
+numbers = {10, 20, 50}
+
+numbers.add(40)
+print("After add:", numbers)
+
+numbers.remove(20)
+print("After remove:", numbers)
+
+numbers.discard(60)
+print("After discard:", numbers)
+
+numbers.clear()
+print("After clear:", numbers)
