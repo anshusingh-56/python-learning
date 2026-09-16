ee72b671110c59d972cace44801f9d8a7efe754a
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-16 21:21:02 +0530

    Add nested dictionary practice

---
+++ dictionaries/nested_dictionary.py
@@ -0,0 +1,20 @@
+students = {
+    "student1": {
+        "name": "Anshu",
+        "age": 18,
+        "course": "B.Tech CSE"
+    },
+    "student2": {
+        "name": "Shikha",
+        "age": 18,
+        "course": "B.Tech CSE"
+    }
+    
+}
+
+print(students)
+
+print("Name of student 1 is: ", students["student1"]["name"])
+print("Name of student 2 is: ", students["student2"]["name"])
+print("Age of student 1 is: ", students["student1"]["age"])
+print("Age of student 2 is: ", students["student2"]["age"])
