a3103a9e3f4a57fedf3a931db4179b7aa44a45df
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-18 20:48:02 +0530

    Add print element using for loop practice

---
+++ loops/print_element_using_for_loop.py
@@ -0,0 +1,9 @@
+# Print the elements of the following list using a loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+for value in nums:
+    print(value)
+    
+
