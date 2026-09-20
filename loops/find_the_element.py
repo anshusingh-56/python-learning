f22c06a0bdca5b0108dfc72e0f45eb9caf6abe22
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-20 22:18:12 +0530

    Add find the element practice

---
+++ loops/find_the_element.py
@@ -0,0 +1,12 @@
+# Search for a number x in this tuple using loop.
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+idx = 0
+x = 64
+
+while idx < len(nums):
+    if(nums[idx] == x):
+        print("Found at idx", idx)
+    idx += 1
