a0c6983c058f4c600d4c29d99ad9cecbfcede199
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-22 23:17:58 +0530

    Add sum of digits practice

---
+++ loops/sum_of_digits.py
@@ -0,0 +1,12 @@
+# Sum of digits using a loop
+
+number = int(input("Enter a number: "))
+
+sum = 0
+
+while number > 0:
+    digit = number % 10
+    sum += digit
+    number = number //10
+
+print("Sum of digits: ", sum)
