e6e7834a42e6eb5b8e717a1634c67fb55b217a2e
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-22 23:17:19 +0530

    Add reverse number practice

---
+++ loops/reverse_number.py
@@ -0,0 +1,12 @@
+# Reverse a number using a loop
+
+number = int(input("Enter a number: "))
+
+reverse = 0
+
+while number > 0:
+    digit = number % 10
+    reverse = reverse * 10 + digit
+    number = number //10
+
+print("Sum of digits: ", reverse)
