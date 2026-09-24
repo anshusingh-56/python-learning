02e485e9e49d1461497c9d9587618f8f76e59a93
Anshu Singh <anshusingh85741640@gmail.com>
2026-09-24 23:40:46 +0530

    Add area and perimeter of rectangle practice

---
+++ basics/area_and_perimeter_of_rectangle.py
@@ -0,0 +1,10 @@
+# Find area and perimeter of rectangle.
+
+l = int(input("Enter length: "))
+w = int(input("Enter width: "))
+
+area = l * w
+perimeter = 2 * ( l + w )
+
+print("Area of rectangle:", area)
+print("Perimeter of rectangle:", perimeter)
